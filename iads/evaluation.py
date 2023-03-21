79e1fb65968f594134837e2738ff7fccb5b0fd01
Paul Jarski <pjarski@sfcm.edu>
2023-03-21 21:08:09 +0100

    added some argumentation, sampling of allw so that the calculations terminate

--- iads/evaluation.py
+++ iads/evaluation.py
@@ -16,3 +16,26 @@ import pandas as pd
 # ------------------------ 
 #TODO: à compléter  plus tard
 # ------------------------ 
+
+def crossval_strat(X, Y, n_iterations, iteration):
+    index_pos = [ i for i in range(len(Y)) if Y[i]==1]
+    index_neg = [ i for i in range(len(Y)) if Y[i]== -1]
+
+    index_pos_test = index_pos[iteration*(len(index_pos) // n_iterations ): (iteration+1)*(len(index_pos) // n_iterations) ]
+    index_neg_test = index_neg[iteration*(len(index_neg) // n_iterations ): (iteration+1)*(len(index_neg) // n_iterations) ]
+    
+    Xtest = np.concatenate(( X[index_neg_test] , X[index_pos_test]  ))
+    Ytest =  np.concatenate(( Y[index_neg_test] , Y[index_pos_test] ))
+    
+    index_app = [i for i in range(len(Y)) if ( (i not in index_pos_test) and (i not in index_neg_test) )]
+
+    Xapp =  X[index_app]
+    Yapp =  Y[index_app]
+    return Xapp, Yapp, Xtest, Ytest
+
+def analyse_perfs(L):
+    """ L : liste de nombres réels non vide
+        rend le tuple (moyenne, écart-type)
+    """
+    return (np.mean(L),np.std(L))
+
